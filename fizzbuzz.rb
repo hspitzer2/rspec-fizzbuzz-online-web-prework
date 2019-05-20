70f2edb7b94a592d9a538df843695cd51fc1d772
Hillary Spitzer <hspitzer2@gmail.com>
2019-05-20 17:00:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@ def fizzbuzz(int)
   
   
   if int % 5 == 0 # if the number int is divisible by 5
-    "buzz" # Go fizz
+    "Buzz" # Go fizz
   else int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   end
